8f642822d7e0def268a5b5ba2f96a153caa1063f
Eilima <ericklima34@gmail.com>
2020-07-27 15:57:30 -0500

    Use hooks for the cityName state/hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { SearchBar } from "./Components/SearchBar/SeachBar";
 import { WeatherResults } from "./Components/WeatherResults/WeatherResults";
 import { YelpResults } from "./Components/YelpResults/YelpResults";
 
 function App() {
+  const [cityName, setCityName] = useState(null);
+
+  const handleCityNameChange = (e) => {
+    setCityName(e.target.value);
+  };
+
   return (
     <div className="App">
-      <SearchBar />
+      <SearchBar handleChange={handleCityNameChange} cityName={cityName} />
       <WeatherResults />
       <YelpResults />
     </div>
