051a233d6575ced0f18edebc44211aa2b6e8a41f
Eilima <ericklima34@gmail.com>
2020-07-27 22:20:38 -0500

    Create component to handle individual business

---
+++ src/Components/BusinessList/BusinessList.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./BusinessList.css";
+
+export const BusinessList = (props) => {
+  return (
+    <div className="Business">
+      <div className="image-container">
+        <a href={props.business.url}>
+          <img src={props.business.imageSrc} alt="" />
+        </a>
+      </div>
+      <h2>{props.business.name}</h2>
+      <div className="Business-information">
+        <div className="Business-address">
+          <p>{props.business.address}</p>
+          <p>{props.business.city}</p>
+          <p>{`${props.business.state} ${props.business.zipCode}`}</p>
+        </div>
+        <div className="Business-reviews">
+          <h3>{props.business.category.toUpperCase()}</h3>
+          <h3 className="rating">{`${props.business.rating} stars`}</h3>
+          <p>{`${props.business.reviewCount} reviews`}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
