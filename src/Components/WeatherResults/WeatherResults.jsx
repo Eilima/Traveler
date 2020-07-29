9a0b4d663a18ba7137a446847da11389f7568e70
Eilima <ericklima34@gmail.com>
2020-07-28 23:09:24 -0500

    Handle error

--- src/Components/WeatherResults/WeatherResults.jsx
+++ src/Components/WeatherResults/WeatherResults.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 
 export class WeatherResults extends React.Component {
+  weatherInformation = () => {
+    if (this.props.weatherInfo.code === null) {
+      console.log(this.props.weatherInfo.code);
+      return <div></div>;
+    } else if (this.props.weatherInfo.code !== 200) {
+      return (
+        <div>
+          <p>ERROR</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h3>Weather</h3>
+          <p>{this.props.weatherInfo.location}</p>
+          <p>{this.props.weatherInfo.temperature}</p>
+          <p>{this.props.weatherInfo.humidity}</p>
+          <p>{this.props.weatherInfo.description}</p>
+        </div>
+      );
+    }
+  };
   render() {
-    return (
-      <div>
-        <p>Weather Results</p>
-        <p>{this.props.weatherInfo.location}</p>
-        <p>{this.props.weatherInfo.temperature}</p>
-        <p>{this.props.weatherInfo.humidity}</p>
-        <p>{this.props.weatherInfo.description}</p>
-      </div>
-    );
+    return <div>{this.weatherInformation()}</div>;
   }
 }
