9243ddd498847cb983fe5e82ced90af122b58b2d
Eilima <ericklima34@gmail.com>
2020-08-04 21:16:53 -0500

    Add comments

--- src/Components/BusinessList/BusinessList.jsx
+++ src/Components/BusinessList/BusinessList.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import "./BusinessList.css";
 
+// Function that returns information about each business individually
 export const BusinessList = (props) => {
   return (
     <div className="Business">
