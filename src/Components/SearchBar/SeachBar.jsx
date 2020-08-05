a74ab3e4762d1086f50109e9a03212440e011bfa
Eilima <ericklima34@gmail.com>
2020-08-04 21:17:00 -0500

    Add comments

--- src/Components/SearchBar/SeachBar.jsx
+++ src/Components/SearchBar/SeachBar.jsx
@@ -3,16 +3,17 @@ import React from "react";
 import "./SearchBar.css";
 
 export class SearchBar extends React.Component {
+  // Initiate state to hold name of city
   state = {
     cityName: null,
   };
-
+  // Funciton that will alter the state value with the current text in the input field
   changeCityName = (e) => {
     this.setState({
       cityName: e.target.value,
     });
   };
-
+  // Function that calls the function stored in props while passing the state of the city name
   handleWeatherSearch = (e) => {
     this.props.searchOpenWeatherMap(this.state.cityName);
     this.props.searchYelp(this.state.cityName);
@@ -23,7 +24,9 @@ export class SearchBar extends React.Component {
     return (
       <div className="search-bar">
         <form>
+          {/* When text entered in input field it will call changeCityName function */}
           <input placeholder="Location/City" onChange={this.changeCityName} />
+          {/* When clicked handleWeatherSearch function will be called */}
           <button onClick={this.handleWeatherSearch}>Submit</button>
         </form>
       </div>
